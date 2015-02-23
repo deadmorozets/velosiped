d532996b7aeda7d2bf68bc8286606ab5423f9a31
deadmorozets <deadmoroz73@gmail.com>
2015-02-23 22:27:46 +0200

    Add New, Edit and Delete behaviour for Order

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,48 @@
+class OrdersController < ApplicationController
+  
+  before_action :find_order, only: [:edit, :update, :destroy]
+
+  def index
+  	@orders = Order.all
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_param)
+    if @order.save
+      redirect_to orders_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @order.update(order_param)
+      redirect_to orders_path
+    else 
+      redirect_to :edit
+    end
+  end
+
+  def destroy
+    @order.destroy
+    redirect_to orders_path
+  end
+
+  private
+
+  def find_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_param
+    params.require(:order).permit(:title, :start_date)
+  end 
+
+end
