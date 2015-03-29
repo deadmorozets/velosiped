314d957a79bf4a4b652a4832c79c705233415084
deadmorozets <deadmoroz73@gmail.com>
2015-03-29 16:56:25 +0300

    Add Occupation controller test

--- test/controllers/occupations_controller_test.rb
+++ test/controllers/occupations_controller_test.rb
@@ -1,29 +1,87 @@
 require 'test_helper'
 
 class OccupationsControllerTest < ActionController::TestCase
-  test "should get new" do
+  
+  def auth
+    sign_in FactoryGirl.create(:user)
+  end
+
+  test "should not get index occupations without authentification" do
+    get :index
+    assert_response 302
+  end 
+
+  test "should get index" do
+    auth
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:occupations)
+  end
+
+  test "should not get create occupation without authentification" do
+    post :create, :occupation => FactoryGirl.attributes_for(:occupation)
+    assert_response 302
+  end 
+
+  test "should create occupation" do
+    auth
+    assert_difference('Occupation.count') do
+      post :create, :occupation => FactoryGirl.attributes_for(:occupation)
+    end
+    assert_redirected_to occupations_path
+  end
+
+  test "should not get new occupations without authentification" do
     get :new
-    assert_response :success
+    assert_response 302
+  end 
+
+  test "new should render correct layout" do
+    auth
+    get :new
+    assert_template layout: "layouts/application", partial: "_entry_occupation_form"
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "should not get edit without authentification" do
+    FactoryGirl.create(:occupation)
+    occupation = Occupation.first
+    get :edit, :occupation => FactoryGirl.create(:occupation)
+    assert_response 302
   end
 
-  test "should get edit" do
-    get :edit
+  test "should edit occupation" do
+    auth
+    get :edit, :id => FactoryGirl.create(:occupation).id
     assert_response :success
+    assert_template layout: "layouts/application", partial: "_entry_occupation_form"
   end
 
-  test "should get update" do
-    get :update
-    assert_response :success
+  test "should not update without authentification" do
+    patch :update, :id => FactoryGirl.create(:occupation).id
+    assert_response 302
   end
 
-  test "should get delete" do
-    get :delete
-    assert_response :success
+  test "should update occupation" do
+    #auth
+    #occupation = FactoryGirl.create(:occupation)
+    #occupation.name = "Updated"
+    #patch :update, :occupation => occupation
+    #assert_redirected_to occupations_path
+  end
+
+  test "should not destroy occupation without authentification" do
+    @occupation = FactoryGirl.create(:occupation)
+    assert_difference('Occupation.count', 0) do
+      delete :destroy, :id => @occupation.id
+    end
+  end
+
+  test "should destroy occupation" do
+    auth
+    @occupation = FactoryGirl.create(:occupation)
+    assert_difference('Occupation.count', -1) do
+      delete :destroy, :id => @occupation.id
+    end
   end
 
 end
--- test/models/operation_test.rb
+++ test/models/operation_test.rb
@@ -7,6 +7,18 @@ class OperationTest < ActiveSupport::TestCase
      assert_not operation.save
    end
 
+  test "should not save operation with negative cost" do
+  	operation = FactoryGirl.build(:operation)
+    operation.cost = -5.0
+    assert_not operation.save
+ 	end
+
+ 	test "should not save operation with negative duration" do
+  	operation = FactoryGirl.build(:operation)
+    operation.duration = -0.5
+    assert_not operation.save
+ 	end
+
    test "should save correct occupation" do
    	operation = FactoryGirl.build(:operation)
     assert operation.save
